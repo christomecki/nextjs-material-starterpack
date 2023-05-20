d4ce682f66bbbca412206c21d468ee63dbcb4f44
grzesiek-git <nowak.grzegorz140@gmail.com>
2023-05-20 10:50:05 +0200

    navigation tests

--- cypress/e2e/app.cy.ts
+++ cypress/e2e/app.cy.ts
@@ -1,7 +1,7 @@
 /// <reference types="cypress" />
 
 describe('Navigation', () => {
-    it('should navigate to the about page', () => {
+    it('should navigate to about page and back to home page', () => {
       // Start from the index page
       cy.visit('/');
    
@@ -14,15 +14,56 @@ describe('Navigation', () => {
       // Nabigation by button "home"
       cy.get('a[href*="/"]').contains('home').click();
 
-      // Navigate to "login" page
-      cy.get('a[href*="/login"]').click();
+      // Navigate to "about" page
+      cy.get('a[href*="/about"]').click();
 
       // Check the login page
-      cy.url().should('include', '/login');
-      cy.get('h1').contains('Sign in');
+      cy.url().should('include', '/about');
 
       // Check a logo navigation
       cy.get('a[href*="/"]').get('img').click();
 
     });
+  
+    it('Navigation on login page, create accound, forgot password', () => {
+      // Start from the index page
+      cy.visit('/');
+   
+      // Navigate to "login" page
+      cy.get('a[href*="/login"]').click();
+   
+      // Check the "login" page
+      cy.url().should('include', '/login');
+
+      // Click on login with empty input fields
+      cy.get('button[type*="submit"]').click();
+
+      // Check if required is working
+      cy.get('p').contains('Email');
+
+      // Check if required is working
+      cy.get('p').contains('Password is required');
+
+      // Check navigation to forgotPassword
+      cy.get('a[href*="/forgotPassword"]').click();
+
+      // Check the "forgotPassword" page
+      cy.url().should('include', '/forgotPassword');
+
+      // Navigate to "login" page
+      cy.get('a[href*="/login"]').click();
+
+      // Navigate to "Create account" page
+      cy.get('a[href*="/signup"]').click();
+
+      // Check the "Create account" page
+      cy.url().should('include', '/signup');
+
+      // Click on "I have arleady account"
+      cy.get('a[href*="/login"]').click();
+
+      // Check the "Create account" page
+      cy.url().should('include', '/login');
+
+    })
   });
